25258b4ee470511b661ce40c688c243ee2ddaf5f
codeitdown <sumit@freightbro.com>
2020-12-29 13:36:54 +0530

    global styled

---
+++ src/components/styles/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: 'Robot', sans-serif;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`;
+
+
