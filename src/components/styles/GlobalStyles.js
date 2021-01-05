c3d882473872b9ff882feb32335c302bead54148
codeitdown <sumit@freightbro.com>
2021-01-05 09:51:53 +0530

    trips, global styles

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
     * {
@@ -7,6 +7,4 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
-`;
-
-
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
+import Trips from "../components/Trips"
+import Testimonials from "../components/Testimonials"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <Hero></Hero>
+    <Trips heading="Our Favorite Destination"></Trips>
+    <Testimonials />
   </Layout>
 )
 
